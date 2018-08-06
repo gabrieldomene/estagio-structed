8dfd117d58361f47154906f30e827483593a5357
Gabriel Domene <domenee.g@gmail.com>
2018-08-06 08:24:52 -0300

    Server modifications to implement the others commit

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express');
+var mongodb = require('mongodb');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 var hbs = require('express-handlebars');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+mongoose.connect('mongodb://localhost:27017/LCC');
 
 // view engine setup
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
@@ -23,5 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// post cases
+app.post('/login', usersRouter);
+app.post('/cadastro-sala', usersRouter);
+app.get('/teste', usersRouter);
+app.get('/getbear', usersRouter);
 
 module.exports = app;
