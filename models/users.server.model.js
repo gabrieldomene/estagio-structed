70eac59b705b58b072d7a42882af4e6f760f02e4
Gabriel Domene <domenee.g@gmail.com>
2018-08-07 11:27:06 -0300

    Modofications in schema, not working

--- models/users.server.model.js
+++ models/users.server.model.js
@@ -1,10 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-module.exports = function(){
-    var User = new Schema({
-        username: String,
-        password: String
-    });
-    return mongoose.model('User', User);
-}
+var userProfile = new Schema({
+    username: String,
+    password: String
+});
+
+var roomProfile = new Schema({
+    descricao:  String,
+    capacidade: String,
+    tipoSala:   String,
+    fator1:     String, 
+    fator2:     String,
+    fator3:     String
+});
+
+var classProfile = new Schema({
+    descricao:      String,
+    fase:           String,
+    oferta:         String,
+    dia:            String,
+    start:          String,
+    tipoSalaTurma:  String,
+    creditos:       String
+});
