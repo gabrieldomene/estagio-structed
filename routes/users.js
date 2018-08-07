38af0c94251d258aa50b0c41a848657444e2f786
Gabriel Domene <domenee.g@gmail.com>
2018-08-07 11:27:17 -0300

    Setting routes responses

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var mongodb = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var urldb = "mongodb://localhost:27017";
 var Users     = require('../models/users.server.model');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/LCC');
+
+//Connect to localhost
+mongoose.connect('mongodb://localhost:27017/LCC');
+
+//Connect to MongoLab sandbox...
+/* 
+mongoose.connect('mongodb://gabrieldomene:teste123@ds018248.mlab.com:18248/lcc'); */
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function(){
+  console.log('DB CONECTADO!');
+  
+})
 
 
 
@@ -23,9 +32,9 @@ router.get('/teste', function(req, res){
   res.render('dashboard',{title: 'APLICACAO'});
 }) */
 
-var User = new Users();
+/* var User = new Users();
 User.username = 'gabrieldomene';
-User.password = 'teste1234';
+User.password = 'teste1234'; */
 
 
 router.post('/login', function(req, res){
@@ -40,16 +49,36 @@ router.post('/login', function(req, res){
         console.log('Not found');
         res.write('Login/senha inválidos');
         res.end();
-      }else{//CASO LOGADO, ENVIAR VIEW DASH
+      }else{//CASO LOGADO, ENVIAR VIEW DASH e criar session
         res.render('dashboard', {title: 'BEM VINDO'});
       }
     });
+
   });
 });
 
 router.post('/cadastrar', function(req, res){
-  res.write(User);
-  res.end();
+  let username = req.body.username;
+  let password = req.body.password;
+  let idcentro = req.body.centroID;
+  console.log(username, password, idcentro);
+  MongoClient.connect(urldb, function(err, db){
+    var dbo = db.db('LCC');
+    var query = {username: username};
+    var myobj = {idcentro: idcentro, username: username, password: password};
+    dbo.collection('Users').findOne(query, function(err, user){
+      if (err) throw new Error(err);
+      if(!user){
+        dbo.collection('Users').insertOne(myobj, function(err, res){
+          if (err) throw err;
+        });
+        res.render('index', {title: 'LOGIN PAGE'});
+      }else{
+        res.write('Usuario ja cadastrado, fazer view');
+        res.end();
+      }
+    });
+  });
 });
 
 
@@ -59,11 +88,29 @@ router.post('/cadastro-sala', function(req, res){
   let capac = req.body.capcSala;
   let tipoSala = req.body.tipoSala;
   let fator1 = req.body.fator1;
+  if(fator1 == '') fator1 = 0;
   let fator2 = req.body.fator2;
+  if(fator2 == '') fator2 = 0;
   let fator3 = req.body.fator3;
+  if(fator3 == '') fator3 = 0;
   
-  
-
-})
+  MongoClient.connect(urldb, function(err, db){
+    var dbo = db.db('LCC');
+    var query = {descricao: desc};
+    var myobj = {descricao: desc, capacidade: capac, tiposala: tipoSala, fator1: fator1, fator2: fator2, fator3: fator3};
+    dbo.collection('Salas').findOne(query, function(err, sala){
+      if (err) throw new Error(err);
+      if(!sala){
+        dbo.collection('Salas').insertOne(myobj, function(err, res){
+          if (err) throw err;
+        });
+        res.render('dashboard', {title: 'Seja Bem Vindo!'});
+      }else{
+        res.write('Sala já cadastrada, fazer view!');
+        res.end();
+      }
+    });
+  });
+});
 
 module.exports = router;
