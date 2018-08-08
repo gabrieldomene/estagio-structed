116591b60db126f591390492135b1e10daf27e52
Gabriel Domene <domenee.g@gmail.com>
2018-08-08 11:25:26 -0300

    Routes improvements

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var hbs = require('express-handlebars');
+var passport = require('passport');
 
 
 var indexRouter = require('./routes/index');
@@ -20,13 +21,22 @@ mongoose.connect('mongodb://localhost:27017/LCC');
 /* mongoose.connect('mongodb://gabrieldomene:teste123@ds018248.mlab.com:18248/lcc'); */
 
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: [
+        __dirname + '/views/partials',
+    ]
+}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
