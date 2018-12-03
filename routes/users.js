0cb02684788b1fc4264d48d89c2c321a4cb51da0
Gabriel Domene <domenee.g@gmail.com>
2018-12-03 10:27:42 -0200

    Change routes for final solution, removed files button

--- routes/users.js
+++ routes/users.js
@@ -61,7 +61,7 @@ router.get('/dashboard', function (req, res) {
   res.redirect('/');
 });
 
-router.post('/login', function (req, res) { //FALTA CRIPTOGRAFAR SENHA + VALIDACAO
+router.post('/login', function (req, res) { 
   let userinput = req.body.username;
   let passinput = req.body.password;
 
@@ -180,7 +180,8 @@ router.post('/cadastrar',
   });
 
 
-router.post('/cadastro-sala', function (req, res) { //FALTA CADASTRAR O ID DA SESSAO!!!!
+
+  router.post('/cadastro-sala', function (req, res) { //FALTA CADASTRAR O ID DA SESSAO!!!!
   let desc = req.body.descSala;
   desc = desc.toUpperCase();
   let capac = req.body.capcSala;
@@ -313,7 +314,9 @@ router.post('/cadastro-turma', function (req, res) { //FALTA CADASTRAR O ID DA S
 
 });
 
-//Rota para montagem do arquivo txt necessário para o algoritmo - SALAS
+
+// COMENTADO ANTIGA MONTAGEM DE ARQUIVOS
+/* //Rota para montagem do arquivo txt necessário para o algoritmo - SALAS
 router.get('/verSalas', authenticationMiddleware(), function (req, res) {
   //Usa o room model definido previamente para fazer a busca no mlab
   let nameToSearch = req.user;
@@ -423,7 +426,7 @@ router.get('/verTurmas', authenticationMiddleware(), function (req, res) {
     msg_successT: 'Arquivo gerado',
     name: req.user
   })
-});
+}); */
 
 //Solução final do ensalamento
 router.get('/solucao', authenticationMiddleware(), (req, res) => {
@@ -439,7 +442,150 @@ router.get('/solucao', authenticationMiddleware(), (req, res) => {
   console.log(comando)
 
 
-  exec(comando, (err, stdout, stderr) => {
+  //O FAMOSO HIGHWAY TO CALLBACKHELL, arrumar nas férias!!
+  let nameToSearch = req.user;
+  console.log(nameToSearch)
+  userModel.findOne({
+    username: nameToSearch
+  }, function (err, user) {
+    roomModel.find({
+      idcentro: user.idcentro
+    }, function (err, result) {
+      if (err) throw err;
+      else {
+        if (!result) {
+          console.log('SEM DADOS');
+          res.end();
+        } else {
+          let conc = ''
+          for (let i = 0; i < result.length; i++) {
+            conc += result[i].descricao + ' ' + result[i].capacidade + ' ' + result[i].tipoSala + ' ' + result[i].fator1 + ' ' + result[i].fator2 + ' ' + result[i].fator3 + '\n';
+          }
+          //Comando caso for separar os txt do script
+          //let file_name = "./outputs/outSala"+user.idcentro+".txt"
+          let file_name = "./algoritmo/outSala" + user.idcentro + ".txt"
+          fs.writeFile(file_name, conc, 'utf8', function (err) {
+            if (err) {             
+              throw new Error(err)
+            } else {
+
+              console.log('----------- ESCRITO ARQUIVO DE SALA -------------');
+              console.log(user)
+              classModel.find({
+                idcentro: user.idcentro
+              }, function (err, result) {
+                if (err) throw new Error(err);
+                else {
+                  if (!result) {
+                    console.log('Não existe dados');
+                  } else {
+                    var final = ""
+                    for (let i = 0; i < result.length; i++) {
+                      let base = '';
+                      base = result[i].descricao + ' ' + result[i].fase + ' ' + result[i].oferta + ' ' + result[i].demanda;
+                      let temp = []
+                      if (result[i].dia.length > 1) {
+                        for (let j = 0; j < result[i].dia.length; j++) {
+                          let temporaria = result[i].dia[j] + ' ' + result[i].start[j] + ' ' + result[i].creditos[j] + ' : ' + result[i].tipoSalaTurma[j];
+                          temp.push(temporaria);
+                        }
+                      } else {
+                        let temporaria = result[i].dia + ' ' + result[i].start + ' ' + result[i].creditos + ' : ' + result[i].tipoSalaTurma;
+                        temp.push(temporaria);
+                      }
+                      let conc = '';
+                      if (temp.length > 1) {
+                        for (let k = 0; k < temp.length; k++) {
+                          if (k == 0) {
+                            conc = '{' + temp[k] + ', ';
+                          } else if (k == (temp.length - 1)) {
+                            conc = conc + temp[k] + '}';
+                          } else {
+                            conc = conc + temp[k] + ', '
+                          }
+                        }
+                      } else {
+                        conc = '{' + temp + '}';
+                      }
+                      final = final + base + ' ' + conc + '\n';
+                      //console.log(final)
+                    }
+                    //Linha caso for separar os txt do script
+                    //let file_name = "./outputs/outTurma"+user.idcentro+".txt"
+                    let file_name = "./algoritmo/outTurma" + user.idcentro + ".txt"
+                    fs.writeFile(file_name, final, function (err) {
+                      if (err) throw new Error(err)
+                      else{
+                        console.log(' -------------- ARQ DE CLAS ESCRITO ----------------');
+                        // CÓDIGO FUNCIONAL PRA BAIXO ------------------------------------------------------
+                         exec(comando, (err, stdout, stderr) => {
+
+                          let transporter = nodemailer.createTransport({
+                            host: 'smtp.gmail.com',
+                            port: 587,
+                            secure: false, // true for 465, false for other ports
+                            auth: {
+                              user: 'lcc.ufsc@gmail.com', // generated ethereal user
+                              pass: 'lccufsc2018' // generated ethereal password
+                            }
+                          });
+
+                          let msg_corpo = 'Olá ' + req.user + '. \n\nA solução encontrada pelo algoritmo de ensalamento está anexada em formato de texto junto a este email. Possíveis salas não alocadas estão anexadas no arquivo statistics e descrevem quais não foram possíveis.\n\nCaso este email não possua 2 (dois) arquivos txt sendo eles o outCentro e statisticsCentro por favor entre em contato.'
+                          // setup email data with unicode symbols
+
+                          let attachments_out = 'outCentro' + req.session.userId + '.txt'
+                          let path_out = './algoritmo/' + attachments_out
+                          console.log(path_out)
+
+                          let attachments_stats = 'statisticsCentro' + req.session.userId + '.txt'
+                          let path_stats = './algoritmo/' + attachments_stats
+                          console.log(path_stats)
+
+                          let mailOptions = {
+                            from: '"LCC Araranguá" <lcc.ufsc@gmail.com>', // sender address
+                            to: 'domenee.g@gmail.com', // list of receivers
+                            subject: 'Resultado do ensalamento',
+                            text: msg_corpo,
+                            attachments: [{
+                                filename: attachments_out,
+                                path: path_out // stream this file
+                              },
+                              {
+                                filename: attachments_stats,
+                                path: path_stats // stream this file
+                              }
+                            ]
+
+                          };
+
+                          // send mail with defined transport object
+                          transporter.sendMail(mailOptions, (error, info) => {
+                            if (error) {
+                              return console.log(error);
+                            }
+                            console.log('Message sent: %s', info.messageId);
+
+                            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                          });
+                          res.render('success', {
+                            title: 'Sucesso'
+                          });
+                        });
+                      }
+                    });
+                  }
+                }
+              });
+            }
+          });
+        }
+      }
+    });
+  });
+
+
+    // CÓDIGO FUNCIONAL PRA BAIXO ------------------------------------------------------
+  /* exec(comando, (err, stdout, stderr) => {
 
     let transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
@@ -491,7 +637,7 @@ router.get('/solucao', authenticationMiddleware(), (req, res) => {
     res.render('success', {
       title: 'Sucesso'
     });
-  });
+  }); */
 });
 
 //Rota para carregar a página de CRUD sala
