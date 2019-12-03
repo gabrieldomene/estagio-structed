cea59426a552b21d33a2ce91cb83f9cb6d1bb928
Gabriel Domene <domenee.g@gmail.com>
2019-12-03 11:40:50 -0200

    New semester field

--- models/class-model.js
+++ models/class-model.js
@@ -1,17 +1,18 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 //Class Schema
-var classProfile = new Schema({
-    descricao: String,
-    fase: String,
-    oferta: String,
-    demanda: String,
-    dia: Array,
-    start: Array,
-    tipoSalaTurma: Array,
-    creditos: Array,
-    idcentro: String
+var classSchema = new Schema({
+    descricao: {type: String, required: true},
+    fase: {type: String},
+    semester: {type: Number},
+    oferta: {type: String},
+    demanda: {type: String},
+    dia: {type: Array},
+    start: {type: Array},
+    tipoSalaTurma: {type: Array},
+    creditos: {type: Array},
+    idcentro: {type: String}
 });
 
-module.exports = mongoose.model('Classes', classProfile);
+module.exports = mongoose.model('Disciplinas', classSchema);
