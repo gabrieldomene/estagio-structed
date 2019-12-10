e62e441ea6f2d22c13ef4517ea86243f9975f37a
Gabriel Domene <domenee.g@gmail.com>
2019-12-10 17:05:02 -0200

    Mongo client option useUnifiedTopology

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {	
-    mongoURI: 'mongodb://***REMOVED***:***REMOVED***@ds149682.mlab.com:49682/node-lcc-google',	
+    mongoURI: 'mongodb://gdomenee:manaogod123@ds149682.mlab.com:49682/node-lcc-google',	
 };
